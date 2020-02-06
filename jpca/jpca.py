7c5a1a1ac2c6fd2382b9b66ecc36987e749b51b8
rguo123 <richardg7890@gmail.com>
2020-02-06 10:44:06 -0800

    jpca seems to be working although some reorienting of jpc steps are skipped

--- jpca/jpca.py
+++ jpca/jpca.py
@@ -17,11 +17,12 @@ def jpca(X, k=6):
     dX = np.diff(X_red, axis=0)
     m_skew = fit_skew(X_prestate, dX)
     M_skew = vec2vec2mat(m_skew)
-
-
+    return get_jpc_pairs(M_skew)
 
 
 def pca_preproc(X, k):
+    # k must be even
+    assert k//2 == k/2
     pca = PCA(n_components=k)
     pca.fit(X)
     X_red = pca.transform(X)
@@ -50,6 +51,30 @@ def optimize_skew(m_skew, X_prestate, dX):
     return optimize.minimize(objective, m_skew,
                                         jac=derivative,
                                         args=(X_prestate, dX))
+
+def get_jpc_pairs(M_skew):
+    evals, evecs = np.linalg.eig(M_star)
+    evecs = evecs.T
+    eval_j = np.imag(evals)
+    # negate to get descending order
+    sort_indices = np.argsort(-np.absolute(eval_j))
+    sorted_evecs = evecs[sort_indices]
+    jpc_pairs = []
+    for i in range(len(sorted_evecs)//2):
+        v1 = sorted_evecs[i]
+        v2 = sorted_evecs[i + 1]
+        conj_pair = (v1, v2)
+        jpcs = get_real_projection_vecs(conj_pair)
+        jpc_pairs.append(jpcs)
+    return jpc_pairs
+
+
+def get_real_projection_vecs(conj_pair, evals=None):
+    #TODO: make all vectors go counter clockwise
+    # TODO: confusing rotation step?
+    v1 = conj_pair[0] + conj_pair[1]
+    v2 = (conj_pair[1] - conj_pair[0])*1j
+    return (np.real(v1), np.real(v2))
 
 
 def mat2vec(mat):
